fa9bbc855e40bca0988e03c29ae296233c4024c0
Kostas Bariotis <konmpar@gmail.com>
2014-11-18 15:13:16 +0200

    notices added to code files

--- src/Block/Success.php
+++ src/Block/Success.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @package    KBariotis_Linkwise
+ * @author     Kostas Bariotis
+ * @copyright  Copyright (c) 2014 Kostas Bariotis (http://www.kostasbariotis.com)
+ * @licence  https://github.com/stakisko/KBariotis_Linkwise/blob/master/LICENCE MIT
+ */
+
 class KBariotis_Linkwise_Block_Success extends Mage_Core_Block_Template
 {
 
